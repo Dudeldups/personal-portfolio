8548a367d9d75f762506677098033764efcbcac8
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2025-04-01 15:40:37 +0200

    add inner div and onScroll effect

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,17 +1,51 @@
+import { useEffect, useRef } from "react";
 import LanguageSwitcher from "./LanguageSwitcher";
 import Navbar from "./Navbar";
 
 const Header = () => {
-  return (
-    <header className="flex w-full max-w-page items-center px-page py-5">
-      <a href="/" className="mr-auto font-oswald text-2xl">
-        <span className="text-primary">A</span>rne{" "}
-        <span className="text-accent">J</span>
-        acob
-      </a>
-      <Navbar />
+  const headerRef = useRef<HTMLDivElement>(null);
 
-      <LanguageSwitcher />
+  useEffect(() => {
+    const handleScroll = () => {
+      if (headerRef.current) {
+        if (window.scrollY > 0) {
+          headerRef.current.classList.add(
+            "bg-dark-light",
+            "shadow-lg",
+            "shadow-darkest",
+          );
+          headerRef.current.classList.remove("bg-darkest");
+        } else {
+          headerRef.current.classList.remove(
+            "bg-dark-light",
+            "shadow-lg",
+            "shadow-darkest",
+          );
+          headerRef.current.classList.add("bg-darkest");
+        }
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      ref={headerRef}
+      className="sticky top-0 z-100 w-full bg-darkest px-page backdrop-blur-sm transition-colors duration-300"
+    >
+      <div className="mx-auto flex max-w-page items-center py-5">
+        <a href="#" className="mr-auto font-oswald text-2xl sm:text-3xl">
+          <span className="text-primary">A</span>rne{" "}
+          <span className="text-accent">J</span>
+          acob
+        </a>
+        <Navbar />
+
+        <LanguageSwitcher />
+      </div>
     </header>
   );
 };
