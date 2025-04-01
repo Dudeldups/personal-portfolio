c771033b0a502e1d7f69cabca7101fb6112afff2
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2025-04-01 15:40:03 +0200

    fix inner div padding

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full bg-dark">
-      <div className="mx-auto max-w-page px-page py-4 text-center md:py-6">
+    <footer className="w-full bg-dark px-page">
+      <div className="mx-auto max-w-page py-4 text-center md:py-6">
         <p>
           &copy; {currentYear} Arne Jacob - {t(KEYS.FOOTER.DISCLAIMER)}
         </p>
