aec5f58c9ee3312c4dcaf753c859af06e58f3ed0
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2025-04-10 17:46:50 +0200

    add h1 and LanguageSwitcher on lg

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -8,21 +8,24 @@ const Header = () => {
   const { t } = useTranslation();
 
   return (
-    <header className="top-0 w-full px-page lg:sticky">
-      <div className="flex w-full flex-col items-center bg-slate-800 py-5 lg:min-h-screen">
-        <LanguageSwitcher />
+    <header className="top-0 w-full shrink-[1.25] px-page lg:sticky">
+      <div className="flex w-full flex-col items-center py-6 lg:min-h-screen lg:items-start lg:pb-14">
+        <LanguageSwitcher className="self-end lg:hidden" />
 
         <div className="mt-8 mb-12 text-center lg:text-left">
-          <h1>
+          <h1 className="">
             <Trans i18nKey={KEYS.INTRO.TITLE}>
-              0 <span className="underline decoration-primary">1</span>2
+              <span>0</span>
+              <br />
+              <span className="underline decoration-primary">2</span>
+              <span>3</span>
             </Trans>
           </h1>
 
           <h2 className="mt-8 text-2xl font-bold text-white sm:text-3xl md:mt-7 lg:text-4xl">
             {t(KEYS.INTRO.JOB)}
           </h2>
-          <p className="mt-6 max-w-md text-lg max-md:mx-auto sm:text-xl md:mt-8">
+          <p className="mt-6 text-lg max-lg:max-w-md sm:text-xl md:mt-8">
             <Trans i18nKey={KEYS.INTRO.DESC}>
               0 <br />2
             </Trans>
@@ -31,11 +34,15 @@ const Header = () => {
 
         <Navbar />
 
-        <ButtonContainer
-          hideText={true}
-          hasRoundedButtons={true}
-          className="mt-auto flex items-center justify-center gap-6 md:justify-start"
-        />
+        <div className="mt-auto flex items-end lg:w-full lg:justify-between">
+          <ButtonContainer
+            hideText={true}
+            hasRoundedButtons={true}
+            className="flex gap-6"
+          />
+
+          <LanguageSwitcher className="max-lg:hidden" />
+        </div>
       </div>
     </header>
   );
