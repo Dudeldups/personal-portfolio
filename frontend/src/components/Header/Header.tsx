18cf7cd8247be149797fec2d4bd81f3ebb71f872
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2025-05-17 23:56:52 +0200

    add h1 animation

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,22 +3,44 @@ import ButtonContainer from "../ButtonContainer/ButtonContainer";
 import Navbar from "./Navbar";
 import LanguageSwitcher from "./LanguageSwitcher";
 import { KEYS } from "../../i18n/KEYS";
+import { useMotionValueEvent, useScroll } from "motion/react";
+import { useState } from "react";
 
 const Header = () => {
   const { t } = useTranslation();
+  const { scrollYProgress } = useScroll();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    if (latest > 0) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  });
 
   return (
-    <header className="top-0 w-full shrink-[1.25] px-page lg:sticky">
+    <header className={`top-0 w-full shrink-[1.25] px-page lg:sticky`}>
       <div className="flex w-full flex-col items-center py-6 lg:min-h-screen lg:items-start lg:pb-14">
         <LanguageSwitcher className="self-end lg:hidden" />
 
-        <div className="mt-8 mb-12 text-center lg:text-left">
+        <div
+          className={`mt-8 mb-12 text-center transition-transform delay-500 lg:text-left ${isScrolled ? "-translate-y-10" : ""}`}
+        >
           <h1 className="">
             <Trans i18nKey={KEYS.INTRO.TITLE}>
-              <span>0</span>
+              <span
+                className={`inline-block transition-transform delay-500 ${isScrolled ? "-translate-y-60" : ""}`}
+              >
+                0
+              </span>
               <br />
               <span className="underline decoration-primary">2</span>
-              <span>3</span>
+              <span
+                className={`inline-block transition-transform delay-500 ${isScrolled ? "-translate-y-60" : ""}`}
+              >
+                3
+              </span>
             </Trans>
           </h1>
 
