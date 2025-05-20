68222630dc0396ab290dd6c8f77de621e9940bb6
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2025-05-20 17:06:18 +0200

    add framer animations for nav links

--- frontend/src/components/Header/Navbar.tsx
+++ frontend/src/components/Header/Navbar.tsx
@@ -3,6 +3,19 @@ import { KEYS } from "../../i18n/KEYS";
 import { useInView } from "motion/react";
 import { useRefStore } from "../../stores/refStore";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+const spanVariants = {
+  initial: {
+    width: "3rem",
+    backgroundColor: "var(--color-light)",
+  },
+  visible: {
+    width: "4rem",
+    backgroundColor: "var(--color-primary)",
+    transition: { duration: 0.3 },
+  },
+};
 
 const Navbar = () => {
   const { t } = useTranslation();
@@ -13,7 +26,7 @@ const Navbar = () => {
 
   const isAboutInView = useInView(aboutRef ?? { current: null });
   const isProjectsInView = useInView(projectsRef ?? { current: null }, {
-    margin: "-100% 0px 0px 0px",
+    margin: "-50% 0px 0px 0px",
   });
   const isContactInView = useInView(contactRef ?? { current: null }, {
     amount: "all",
@@ -33,21 +46,33 @@ const Navbar = () => {
     <nav className="max-lg:hidden">
       <ul className="flex flex-col gap-3">
         <li className="flex items-center gap-5">
-          <span
-            className={`h-0.5 w-10 ${currentView === "about" ? "bg-primary" : "bg-white"}`}
-          ></span>
+          <motion.span
+            initial="initial"
+            animate={currentView === "about" ? "visible" : "initial"}
+            variants={spanVariants}
+            transition={{ duration: 0.3 }}
+            className={`h-0.5`}
+          />
           <a href="#about">{t(KEYS.NAV.ABOUT)}</a>
         </li>
         <li className="flex items-center gap-5">
-          <span
-            className={`h-0.5 w-10 ${currentView === "projects" ? "bg-primary" : "bg-white"}`}
-          ></span>
+          <motion.span
+            initial="initial"
+            animate={currentView === "projects" ? "visible" : "initial"}
+            variants={spanVariants}
+            transition={{ duration: 0.3 }}
+            className={`h-0.5`}
+          />
           <a href="#projects">{t(KEYS.NAV.PROJECTS)}</a>
         </li>
         <li className="flex items-center gap-5">
-          <span
-            className={`h-0.5 w-10 ${currentView === "contact" ? "bg-primary" : "bg-white"}`}
-          ></span>
+          <motion.span
+            initial="initial"
+            animate={currentView === "contact" ? "visible" : "initial"}
+            variants={spanVariants}
+            transition={{ duration: 0.3 }}
+            className={`h-0.5`}
+          />
           <a href="#contact">{t(KEYS.NAV.CONTACT)}</a>
         </li>
       </ul>
