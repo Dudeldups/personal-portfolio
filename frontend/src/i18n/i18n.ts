b237654a998a00e1c35c5e43d5f09f4b6a5cefe5
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-09 22:08:48 +0200

    refactor env loading

--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -19,7 +19,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    debug: process.env.NODE_ENV === "development",
+    debug: import.meta.env.DEV,
 
     resources,
     detection: {
