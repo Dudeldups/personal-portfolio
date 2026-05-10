bc8e98c6ec8e9f2fb8790462febf73cd40fbb777
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-10 13:18:27 +0200

    add separate HomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,13 @@
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Intro from "./components/Intro/Intro";
-import Projects from "./components/Projects/Projects";
-import Skills from "./components/Skills/Skills";
 import Meta from "./Meta";
+import { Outlet } from "react-router-dom";
 import "./i18n/i18n";
 
 function App() {
   return (
-    <div className="mx-auto flex min-h-screen max-w-page flex-col items-start justify-center lg:flex-row">
+    <>
       <Meta />
-
-      <Header />
-
-      <div className="">
-        <main className="">
-          <Intro />
-          <Skills />
-          <Projects />
-          <Contact />
-        </main>
-
-        <Footer />
-      </div>
-    </div>
+      <Outlet />
+    </>
   );
 }
 
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,27 @@
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Intro from "../components/Intro/Intro";
+import Projects from "../components/Projects/Projects";
+import Skills from "../components/Skills/Skills";
+
+const HomePage = () => {
+  return (
+    <div className="mx-auto flex min-h-screen max-w-page flex-col items-start justify-center lg:flex-row">
+      <Header />
+
+      <div>
+        <main>
+          <Intro />
+          <Skills />
+          <Projects />
+          <Contact />
+        </main>
+
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
