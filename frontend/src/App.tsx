a247d24527ccc163640688d17a6a284db210f083
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-10 23:03:57 +0200

    remove meta component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,8 @@
-import Meta from "./Meta";
 import { Outlet } from "react-router-dom";
 import "./i18n/i18n";
 
 function App() {
-  return (
-    <>
-      <Meta />
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 }
 
 export default App;
