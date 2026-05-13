d3c48b79eb7e119dab53ad3b836a0ff990e1ad9a
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-13 22:58:16 +0200

    add scroll to top on route change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import "./i18n/i18n";
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
+
   return <Outlet />;
 }
 
