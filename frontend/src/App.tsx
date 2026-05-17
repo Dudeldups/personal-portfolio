2e5cec4200095204f54fb1b8c756ade3906a01ce
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-17 13:18:34 +0200

    add useEffect for document lang attribute

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { Outlet, useLocation } from "react-router-dom";
 import "./i18n/i18n";
 
 function App() {
   const { pathname } = useLocation();
+  const { i18n } = useTranslation();
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "instant" });
   }, [pathname]);
+
+  useEffect(() => {
+    document.documentElement.lang = i18n.resolvedLanguage ?? i18n.language;
+  }, [i18n.language, i18n.resolvedLanguage]);
 
   return <Outlet />;
 }
